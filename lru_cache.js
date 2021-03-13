9720e1dcc868f52d72d18e6b0a47885fc1d1fd4c
kennethguan1 <kennethguan1@gmail.com>
2021-03-12 20:47:52 -0800

    LRU cache

--- lru_cache.js
+++ lru_cache.js
@@ -101,3 +101,37 @@ LRUCache.prototype.put = function(key, value) {
     this.count--;
   }
 };
+
+
+
+
+// alternate:
+
+
+var LRUCache = function(capacity) {
+    this.cache = new Map()
+    this.capacity = capacity
+};
+
+
+LRUCache.prototype.get = function(key) {
+    if(!this.cache.has(key)) return -1
+    
+    const value = this.cache.get(key)
+    this.cache.delete(key)
+    this.cache.set(key, value)
+    
+    return this.cache.get(key)  
+};
+
+LRUCache.prototype.put = function(key, value) {
+    if(this.cache.has(key)){
+        this.cache.delete(key)
+    }
+    
+    this.cache.set(key, value)
+    if(this.cache.size > this.capacity){
+        this.cache.delete(this.cache.keys().next().value)
+    }
+    
+};
